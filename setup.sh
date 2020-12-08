8e3470567fbbf52cdc5242f2ee3c5c5697df0b5f
Frodo Barbera <fbarbera@ox-r3.msk.21-school.ru>
2020-12-08 19:38:21 +0300

    fff

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ docker build -t nginx srcs/nginx/.
 kubectl apply -f ./srcs/nginx/nginx-service.yaml
 kubectl apply -f ./srcs/nginx/nginx-deployment.yaml
 
-# build mysql
+# # build mysql
 docker build -t mysql ./srcs/mysql/
 
 kubectl apply -f ./srcs/mysql/mysql-deployment.yaml
@@ -27,8 +27,16 @@ kubectl apply -f ./srcs/wordpress/wordpress-service.yaml
 
 # build phpmyadmin
 docker build -t phpmyadmin ./srcs/phpmyadmin/
-
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin-deployment.yaml
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin-service.yaml
+
+docker build -t influxdb ./srcs/influxdb/
+kubectl apply -f srcs/influxdb/influxdb.yaml
+kubectl apply -f srcs/influxdb/influxdb-pv.yaml
+# build grafana
+docker build -t grafana ./srcs/grafana
+kubectl apply -f ./srcs/grafana/grafana-deployment.yaml
+kubectl apply -f ./srcs/grafana/grafana-service.yaml
+
 
 minikube dashboard
---
+++ srcs/grafana/srcs/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+openrc default
+./bin/grafana-server
